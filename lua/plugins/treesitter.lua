6883908e63cefb707da6d477a8f4d060e31cfbdf
Aman Babu Hemant <amanbabuhemant@gmail.com>
2025-10-07 08:42:06 +0530

    Set auto_install in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,16 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "markdown", "markdown_inline" },  -- Install Lua parser
-        highlight = { enable = true },  -- Enable syntax highlighting
-      })
-    end
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "markdown", "markdown_inline" },  -- Install Lua parser
+                highlight = {
+                    enable = true,  -- Enable syntax highlighting
+                    additional_vim_regex_highlighting = { "htmldjango" },
+                },
+                auto_install = true, -- Install missing parser on fly
+            })
+        end
+    }
 }
