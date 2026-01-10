a7ee2cc8915dbb49cb4ace64a943e6298315e5f6
Aman Babu Hemant <amanbabuhemant@gmail.com>
2026-01-10 08:58:02 +0530

    Improve toggle bool helper

--- lua/helpers/toggle-bool.lua
+++ lua/helpers/toggle-bool.lua
@@ -2,22 +2,79 @@
 
 local M = {}
 
+M.map = {
+
+    -- true/false
+    ["true"]  = "false",
+    ["false"] = "true",
+
+    -- True/False
+    ["True"]  = "False",
+    ["False"] = "True",
+
+    -- TRUE/FALSE
+    ["TRUE"]  = "FALSE",
+    ["FALSE"] = "TRUE",
+
+    -- yes/no
+    ["yes"]   = "no",
+    ["no"]    = "yes",
+
+    -- Yes/No
+    ["Yes"]   = "No",
+    ["No"]    = "Yes",
+
+    -- YES/NO
+    ["YES"]   = "NO",
+    ["NO"]    = "YES",
+
+    -- y/n
+    ["y"]     = "n",
+    ["n"]     = "y",
+
+    -- Y/N
+    ["Y"]     = "N",
+    ["N"]     = "Y",
+
+    -- on/off
+    ["on"]    = "off",
+    ["off"]   = "on",
+
+    -- On/Off
+    ["On"]    = "Off",
+    ["Off"]   = "On",
+
+    -- ON/OFF
+    ["ON"]    = "OFF",
+    ["OFF"]   = "ON",
+
+    -- 1/0
+    ["1"]     = "0",
+    ["0"]     = "1",
+}
+
 function M.toggle_bool_value(bool)
-    if bool == "true" then return "false" end
-    if bool == "false" then return "true" end
-    if bool == "True" then return "False" end
-    if bool == "False" then return "True" end
-    if bool == "1" then return "0" end
-    if bool == "0" then return "1" end
+    return M.map[bool]
 end
 
 function M.toggle_bool()
+    local cursor_start_pos = vim.api.nvim_win_get_cursor(0)
     vim.cmd(":normal! yiw")
     local bool_value = vim.fn.getreg("")
     local toggled_value = M.toggle_bool_value(bool_value)
+    if not toggled_value then
+        vim.api.nvim_win_set_cursor(0, cursor_start_pos)
+        return
+    end
     vim.fn.setreg("", toggled_value)
     vim.cmd(":normal! viw")
     vim.cmd(":normal! p")
+    local cursor_current_pos = vim.api.nvim_win_get_cursor(0)
+    if cursor_start_pos[2] < cursor_current_pos[2] then
+        vim.api.nvim_win_set_cursor(0, cursor_start_pos)
+    else
+        vim.api.nvim_win_set_cursor(0, cursor_current_pos)
+    end
 end
 
 vim.keymap.set("n", "g!", M.toggle_bool)
