30ad7707e6f7b2dadadd94cd9a8a97eb7d907dec
Aman Babu Hemant <amanbabuhemant@gmail.com>
2026-03-20 09:00:27 +0530

    Custmize lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,35 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require("lualine").setup()
-    end,
+    opts = {
+        options = {
+            icons_enabled = false,
+            component_separators = { left = "┊", right = "┊" },
+            section_separators = { left = "🬻◣🬞", right = "🬏◢🬺"},
+        },
+        sections = {
+            lualine_a = {
+                {
+                    'mode',
+                    fmt = function(str)
+                        while #str < 7 do
+                            str = str .. " "
+                        end return str end
+                }
+            },
+            lualine_b = {},
+            lualine_c = { "filename", "diff"},
+            lualine_x = { "filetype", "diagnostics", 'encoding'},
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+    },
 }
